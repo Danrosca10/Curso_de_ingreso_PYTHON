e07d48faf09ee59de777fee632eeb4c5de66af70
Danilo <danilo.scapi@gmail.com>
2024-02-06 12:13:52 -0300

    ejercicio 8 completo

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_08.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_08.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Danilo 
+apellido: Scapicchi
 ---
 Ejercicio: entrada_salida_08
 ---
@@ -40,7 +40,12 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        operador_a_texto = self.txt_operador_a.get()
+        operador_b_texto = self.txt_operador_b.get()
+        operador_a_numero = int(operador_a_texto)
+        operador_b_numero = int(operador_b_texto)
+        resultado_numero = operador_a_numero / operador_b_numero
+        alert(title="calculadora", message=resultado_numero)
         
         
     
